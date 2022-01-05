6bd466eb74614af437a3e126d6c5e47bba601669
Kyle <kyle@Kyles-MacBook-Air.local>
2022-01-05 20:58:30 +0800

    c

--- copy.sh
+++ copy.sh
@@ -5,7 +5,7 @@ SHELL_DIR=$(dirname "$BASH_SOURCE")
 APP_DIR=$(cd $SHELL_DIR; pwd)
 cd $APP_DIR
 
-cp -R ./build /opt/homebrew/var/www/
+cp -Rf ./build /opt/homebrew/var/www/
 
 # nginx
 # http://localhost:8080/build/
